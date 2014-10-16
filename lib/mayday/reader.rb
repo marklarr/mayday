2fdf60818a78e8ef53a43accb603a249e0308c3e
Mark Larsen <larse503@gmail.com>
2014-10-16 15:10:37 -0500

    [Additional Features] Allow users to provide warnings on a per-line, per-file, or per-line with regex basis.

--- lib/mayday/reader.rb
+++ lib/mayday/reader.rb
@@ -32,15 +32,63 @@ module Mayday
       @xcode_proj = Xcodeproj::Project.open(real_xcodeproj_path)
     end
 
-    def warning(&block)
-      @build_phase_generator.flags << Warning.new(block)
+    def warning_regex(message, regex)
+      abstract_flag_regex(Warning, message, regex)
+    end
+
+    def error_regex(message, regex)
+      abstract_flag_regex(Error, message, regex)
+    end
+
+    def abstract_flag_regex(klass, message, regex)
+      block = <<-CODE
+lambda do |line|
+  line =~ Regexp.new('#{regex}') ? "#{message}" : nil
+end
+      CODE
+      abstract_flag(klass, :line, block)
+    end
+
+    def warning(type=:line, &block)
+      abstract_flag(Warning, type, block)
     end
     private :warning
 
-    def error(&block)
-      @build_phase_generator.flags << Error.new(block)
+    def error(type=:line, &block)
+      abstract_flag(Error, type, block)
     end
     private :error
+
+    def abstract_flag(klass, type=:line, block)
+      block_str = case block
+      when String
+        block
+      when Proc
+        block.to_source
+      else
+        raise TypeError, "#{self.class}'s block has invalid type of #{@block.class}"
+      end
+
+      final_block = case type
+                    when :line
+                      <<-CODE 
+lambda do |file_contents|
+  hash = {}
+  lines = file_contents.split("\n")
+  lines.each_with_index do |line, line_number| 
+    message = #{block_str}.call(line)
+    hash[line_number + 1] = message if message
+  end
+  hash
+end
+                        CODE
+                    when :file
+                      block
+                    end
+
+      @build_phase_generator.flags << klass.new(final_block)
+    end
+    private :abstract_flag
 
   end
 end
