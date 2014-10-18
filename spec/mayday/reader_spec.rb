aada0923ea2fde5ea193c32b0ab780e8781a5a04
Mark Larsen <larse503@gmail.com>
2014-10-18 11:46:17 -0700

    Flesh out tests to more specs

--- spec/mayday/reader_spec.rb
+++ spec/mayday/reader_spec.rb
@@ -4,6 +4,7 @@ describe Mayday::Reader do
 
   def parse_build_output
     output =`xcodebuild -project spec/test_fixtures/Fixtures/Fixtures.xcodeproj/ -scheme Fixtures -configuration Debug`
+    exitstatus = $?.exitstatus
     files_to_lines_to_warnings_hash = {}
     output.split("\n").each do |line|
       matches = line.match(/((.+):([0-9]+):\s(.+))/)
@@ -18,15 +19,19 @@ describe Mayday::Reader do
       end
     end
 
-    return files_to_lines_to_warnings_hash
+    { :exitstatus => exitstatus, :files_to_lines_to_warnings_hash => files_to_lines_to_warnings_hash }
   end
+
+  before(:all) do
+      user_definitions = Mayday::UserDefinitions.new(FIXTURES_TEST_MAYDAY_FILE_PATH)
+      user_definitions.up
+      @parsed_build_output = parse_build_output
+  end
+
+  let(:files_to_lines_to_warnings_hash) { @parsed_build_output[:files_to_lines_to_warnings_hash] }
 
   describe "read" do
     it "should add a build phase run script to the project" do
-      user_definitions = Mayday::UserDefinitions.new(FIXTURES_TEST_MAYDAY_FILE_PATH)
-      user_definitions.up
-      files_to_lines_to_warnings_hash = parse_build_output
-
       expect(files_to_lines_to_warnings_hash.count).to eq(2)
 
       some_object_h_flags = files_to_lines_to_warnings_hash['/Users/marklarsen/github.com/mayday/spec/test_fixtures/Fixtures/Fixtures/SomeDir/SomeObject.h']
@@ -37,6 +42,23 @@ describe Mayday::Reader do
 
 
     end
+
+    it "should have failed the build" do
+      expect(@parsed_build_output[:exitstatus]).to_not eq(0)
+    end
+
+    it "should have output all files with warnings or errors in them" do
+      expect(files_to_lines_to_warnings_hash.count).to eq(2)
+    end
+
+    it "should have, for every file, output all of the warnings or errors in them" do
+      some_object_h_flags = files_to_lines_to_warnings_hash['/Users/marklarsen/github.com/mayday/spec/test_fixtures/Fixtures/Fixtures/SomeDir/SomeObject.h']
+      app_delegate_swift_flags = files_to_lines_to_warnings_hash['/Users/marklarsen/github.com/mayday/spec/test_fixtures/Fixtures/Fixtures/AppDelegate.swift']
+
+      expect(some_object_h_flags.count).to eq(1)
+      expect(app_delegate_swift_flags.count).to eq(9)
+    end
+
   end
 
 end
